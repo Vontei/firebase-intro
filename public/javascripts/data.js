d1ef9b82660a85840fb3b0c527572a882393f76a
Nick Mattei <nmattei89@gmail.com>
2015-09-14 11:59:01 -0600

    working

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,21 +1,18 @@
 // var myDataRef = new Firebase('https://popping-inferno-2304.firebaseio.com/');
 var myData = new Firebase('https://popthis.firebaseio.com/messages');
-    $('#messageInput').keypress(function (e) {
-      if (event.keyCode == 13) {
-        alert('hello')
-        console.log(name)
-        var name = $('#nameInput').val();
-        var text = $('#messageInput').val();
-        myData.push({name: name, text: text});
-        $('#messageInput').val('');
-      }
-    });
-    myData.on('child_added', function(snapshot) {
-      var message = snapshot.val();
-      var name = snapshot.key()
-      displayChatMessage(name, message);
-    });
-    function displayChatMessage(name, text) {
-      $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
-      $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-    };
+$("#messageInput").keypress(function (e) {
+  if (e.keyCode == 13) {
+    var name = $('#nameInput').val();
+    var text = $('#messageInput').val();
+    myData.push({name: name, text: text});
+    $('#messageInput').val('');
+  }
+});
+myData.on('child_added', function(snapshot) {
+  var message = snapshot.val();
+  displayChatMessage(message.name, message.text);
+});
+function displayChatMessage(name, text) {
+  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+};
