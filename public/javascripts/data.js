b73d89b5706638958153b6b8af9dad39d0d15955
Nick Mattei <nmattei89@gmail.com>
2015-09-13 20:25:16 -0600

    keypress broken

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,11 +1,13 @@
-// var Firebase = require('firebase')
-var myDataRef = new Firebase('https://popping-inferno-2304.firebaseio.com/');
+// var myDataRef = new Firebase('https://popping-inferno-2304.firebaseio.com/');
+var myDataRef = new Firebase('https://popthis.firebaseio.com/messages');
     $('#messageInput').keypress(function (e) {
       if (e.keyCode == 13) {
+        alert('hello')
+        console.log(name)
         var name = $('#nameInput').val();
         var text = $('#messageInput').val();
-        myDataRef.push({name: name, text: text});
-        // $('#messageInput').val('');
+        myDataRef.set({name: name, text: text});
+        $('#messageInput').val('');
       }
     });
     myDataRef.on('child_added', function(snapshot) {
