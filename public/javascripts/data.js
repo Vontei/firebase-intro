52ae0e120a7c37bc6abc40be39089409b45cc0d2
Nick Mattei <nmattei89@gmail.com>
2015-09-13 22:26:49 -0600

    Color

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,16 +1,16 @@
 // var myDataRef = new Firebase('https://popping-inferno-2304.firebaseio.com/');
-var myDataRef = new Firebase('https://popthis.firebaseio.com/messages');
+var myData = new Firebase('https://popthis.firebaseio.com/messages');
     $('#messageInput').keypress(function (e) {
-      if (e.keyCode == 13) {
+      if (event.keyCode == 13) {
         alert('hello')
         console.log(name)
         var name = $('#nameInput').val();
         var text = $('#messageInput').val();
-        myDataRef.set({name: name, text: text});
+        myData.push({name: name, text: text});
         $('#messageInput').val('');
       }
     });
-    myDataRef.on('child_added', function(snapshot) {
+    myData.on('child_added', function(snapshot) {
       var message = snapshot.val();
       var name = snapshot.key()
       displayChatMessage(name, message);
